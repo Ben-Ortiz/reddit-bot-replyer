bb6ec42a8242a1b1f63953a84178981471ffa848
Ben Ortiz <44016398+benortiz1391@users.noreply.github.com>
2019-12-26 16:12:05 -0500

    Add files via upload

---
+++ redditbotreplyer.py
@@ -0,0 +1,37 @@
+import sys
+import traceback
+
+import praw
+
+BOT_USERNAME = 'ENTER_HERE'
+BOT_PASSWORD = 'ENTER_HERE'
+BOT_CLIENT_ID = 'ENTER_HERE'
+BOT_CLIENT_SECRET = 'ENTER_HERE'
+
+TARGET_SUBREDDIT = 'ENTER_HERE' #example: TARGET_SUBREDDIT = 'funny'
+TARGET_USER = 'ENTER_HERE' 
+MESSAGE = "ENTER_HERE"
+
+
+def harass():
+    reddit = praw.Reddit(username=BOT_USERNAME,
+                         password=BOT_PASSWORD,
+                         client_id=BOT_CLIENT_ID,
+                         client_secret=BOT_CLIENT_SECRET,
+                         user_agent='UserHarasser v0.1 by /u/throwaway_the_fourth for /u/testmatchelitist. See https://redd.it/6ulado.')
+
+    try:
+        for comment in reddit.subreddit(TARGET_SUBREDDIT).stream.comments():
+            if not comment.saved and comment.author == reddit.redditor(TARGET_USER):
+                comment.save()
+                comment.reply(MESSAGE)
+    except BaseException as e:
+        if type(e) == KeyboardInterrupt:
+            sys.exit(0)
+        else:
+            traceback.print_exc()
+
+
+if __name__ == '__main__':
+    while True:
+        harass()
